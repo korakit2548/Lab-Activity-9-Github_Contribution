a7867e13526b501075879bf128008ad6477e959a
Bell_suwat <bellsuwat2@gmail.com>
2025-09-30 15:32:07 +0700

    position
    เพิ่ม vecter2 position

--- Lab Activity 9-Github_Contribution/Game1.cs
+++ Lab Activity 9-Github_Contribution/Game1.cs
@@ -9,6 +9,7 @@ namespace Lab_Activity_9_Github_Contribution
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         int room = 5;
+        Vector2 _position = Vector2.Zero;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
